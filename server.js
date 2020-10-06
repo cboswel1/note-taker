2c0105eaa7c510006d01eaac7458991ad14e5e07
cboswel1 <christianboswell86@gmail.com>
2020-10-05 19:40:15 -0600

    changed to req.body for json parse)

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //List needed Dependencies 
 
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var uuid = require("uuid");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
 
 
 const app = express();
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -33,14 +33,15 @@ app.get("/api/notes", (req, res) => {
 //POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post("/api/notes", (req, res) => {
   
-  //new note 
-  const newNote = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+  //new note //req.body?
+  const newNote = req.body;
   console.log(newNote);
 
   newNote.push ({
+    //unique id
     id: uuid(),
-    //// req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
     title: req.body.title,
     text: req.body.text
   });
@@ -52,6 +53,8 @@ app.post("/api/notes", (req, res) => {
 
 //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 app.delete("/api/notes/:id", (req, res) => {
+  const newNote = req.body;
+  
 
 });
 
