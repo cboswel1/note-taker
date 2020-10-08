1acdd19c73bc9adce5bc48a14501192d4fa50943
cboswel1 <christianboswell86@gmail.com>
2020-10-08 11:37:10 -0600

    .delete issue in api.js

--- routing/api.js
+++ routing/api.js
@@ -43,9 +43,9 @@ module.exports = function(app) {
   
     //DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
     app.delete("/api/notes/:id", (req, res) => {
-        const newNote = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+        const newNote = noteContent();
         
-        newNote = newNote.filter(note => note.id !== req.params.id);
+        newNote = newNote.filter((note) => note.id !== req.params.id);
 
         fs.writeFileSync("./db/db.json", JSON.stringify(newNote, null, "\t"));
 
